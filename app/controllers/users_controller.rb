dadf16ee88a2fcf714484e3bbf0959b148ccea76
Amsel1676 <kuro.amsel@gmail.com>
2019-12-09 23:11:05 +0000

    Add processing to redirect to user page when user registration is successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #成功時の処理
+      redirect_to @user
     else
       render 'new'
     end
