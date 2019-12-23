32b5259cafbdb13b59ad649721150dbf952fd348
Amsel1676 <kuro.amsel@gmail.com>
2019-12-23 23:29:59 +0000

    Add function to activate account in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  validates     :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  attr_accessor :remember_token
   
   class << self
     def digest(string)
@@ -34,4 +35,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+  
+    #メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    #有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
